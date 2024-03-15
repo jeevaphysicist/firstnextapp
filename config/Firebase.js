2c6cae856afaec6800053e7f980dbd90228e299c
jeevaphysicist <jeevaphysicist@gmail.com>
2024-03-15 12:53:47 +0530

    code submission

--- config/Firebase.js
+++ config/Firebase.js
@@ -1,11 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth ,GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+ 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -20,10 +18,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
-export const auth = getAuth();
-export const provider = new GoogleAuthProvider();
+// export const auth = getAuth();
+// export const provider = new GoogleAuthProvider();
 
 // Export the initialized Firebase app and analytics
 export { app, db };
